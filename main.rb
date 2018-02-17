dc92eebc188c69759737c09539677f6b233c5f74
Safisyx <safisyx@email.com>
2018-02-17 20:02:35 +0100

    Map the contacts to a different shape

--- main.rb
+++ main.rb
@@ -4,3 +4,12 @@ def read_contacts
   contacts_json = File.read("contacts.json")
   return JSON.parse(contacts_json, { symbolize_names: true })
 end
+
+def change_shape(contacts)
+  contacts.map do |contact|
+    {contact[:name]=> contact[:phone]}
+  end
+end
+
+contacts = read_contacts
+new_shape = change_shape(contacts)
