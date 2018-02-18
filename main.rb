dffe35d81cb57e1aa1ebb1fdee2d3ab75e90d820
Safisyx <safisyx@email.com>
2018-02-18 14:56:10 +0100

    Filter the contacts by part of phone and email

--- main.rb
+++ main.rb
@@ -11,5 +11,25 @@ def change_shape(contacts)
   end
 end
 
+def filter_phone(contacts, to_find)
+  contacts.select do |contact|
+    contact[:phone].include? to_find
+  end
+end
+
+def filter_email(contacts, to_find)
+  contacts.select do |contact|
+    contact[:email].include? to_find
+  end
+end
+
 contacts = read_contacts
 new_shape = change_shape(contacts)
+print new_shape
+puts
+contacts_filter_by_phone = filter_phone(contacts, "+1")
+print contacts_filter_by_phone
+puts
+contacts_filter_by_email = filter_email(contacts, ".org")
+print contacts_filter_by_email
+puts
