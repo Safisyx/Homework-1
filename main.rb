4f0340c0b61d02b81b0407418e9802253c0bc0fc
Safisyx <safisyx@email.com>
2018-02-18 15:10:13 +0100

    Count contacts filtered by phone and by email

--- main.rb
+++ main.rb
@@ -23,6 +23,21 @@ def filter_email(contacts, to_find)
   end
 end
 
+def count_phone(contacts, to_find)
+  new_contacts = filter_phone(contacts, to_find)
+  new_contacts.reduce(0) do |sum, element|
+    sum+=1
+  end
+end
+
+def count_email(contacts, to_find)
+  new_contacts = filter_email(contacts, to_find)
+  new_contacts.reduce(0) do |sum, element|
+    sum+=1
+  end
+end
+
+
 contacts = read_contacts
 new_shape = change_shape(contacts)
 print new_shape
@@ -32,4 +47,10 @@ print contacts_filter_by_phone
 puts
 contacts_filter_by_email = filter_email(contacts, ".org")
 print contacts_filter_by_email
+puts
+count_by_phone = count_phone(contacts, "+1")
+print count_by_phone
+puts
+count_by_email = count_email(contacts, ".org")
+print count_by_phone
 puts
